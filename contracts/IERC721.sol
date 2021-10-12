db8041406831daddd78bbaae272a7d0d5383188b
EmmerichS <emschmolli@hotmail.com>
2021-10-12 17:23:24 +0200

    Added balanceOf() and totalSupply() implementations

--- contracts/IERC721.sol
+++ contracts/IERC721.sol
@@ -1,4 +1,5 @@
-pragma solidity ^0.5.0;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
 /**
  * @dev Required interface of an ERC721 compliant contract.
  */
--- contracts/Kittycontract.sol
+++ contracts/Kittycontract.sol
@@ -3,6 +3,22 @@ pragma solidity ^0.8.9;
 
 import "./IERC721.sol";
 
-contract Kittycontract is IERC721 {
+//Contractsare considered abstract if at least ONE of their functions is not implemented
+
+abstract contract Kittycontract is IERC721 {
+
+    constructor() IERC721() {}
+
+    uint allNFTs;
+
+    mapping(address => uint) NFTamount;
+
+    function balanceOf(address owner) external view returns (uint256 balance) {
+        return NFTamount[owner];
+    }
+
+    function totalSupply() external view returns (uint256 total) {
+        return allNFTs;
+    }
 
 }
