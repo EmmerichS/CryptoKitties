06a3537ff01b35c901b028f872848341ff4bff51
EmmerichS <emschmolli@hotmail.com>
2021-10-13 18:05:27 +0200

    Added transfer()

--- contracts/Kittycontract.sol
+++ contracts/Kittycontract.sol
@@ -9,24 +9,21 @@ abstract contract Kittycontract is IERC721 {
 
     constructor() IERC721() {}
 
-    uint numberOfNFTs;
-    //uint[] tokenIDs;
-    Token[] allTokens;
+    uint numberOfTokens;
 
     struct Token {
         address owner;
         uint NFTId;
-        uint structId;
         string name;
         string symbol;
     }
+    Token[] allTokens;
 
-    mapping(address => uint) NFTamount;
-    mapping(address => Token) token;
+    mapping(address => uint) tokenAmount;
     mapping(address => Token[]) allTokensPerOwner;
 
     function balanceOf(address owner) external view returns (uint256 balance) {
-        return NFTamount[owner];
+        return tokenAmount[owner];
     }
 
     function allNFTsPerOwner() public view returns (Token[] memory) {
@@ -34,38 +31,31 @@ abstract contract Kittycontract is IERC721 {
     }
 
     function totalSupply() external view returns (uint256 total) {
-        return numberOfNFTs;
+        return numberOfTokens;
     }
 
-    function name() external view returns (string memory tokenName) {
-        return token[msg.sender].name;
+    function name(uint tokenId) external view returns (string memory tokenName) {
+        return allTokens[tokenId].name;
     }
 
-    function symbol() external view returns (string memory tokenSymbol) {
-        return token[msg.sender].symbol;
+    function symbol(uint tokenId) external view returns (string memory tokenSymbol) {
+        return allTokens[tokenId].symbol;
     }
 
     function ownerOf(uint256 tokenId) external view returns (address owner) {
         return allTokens[tokenId].owner;
     }
 
-    //What about multiple tokens????
-
-    /* @dev Transfers `tokenId` token from `msg.sender` to `to`.
-     *
-     *
-     * Requirements:
-     *
-     * - `to` cannot be the zero address.
-     * - `to` can not be the contract address.
-     * - `tokenId` token must be owned by `msg.sender`.
-     *
-     * Emits a {Transfer} event.
-     */
     function transfer(address to, uint256 tokenId) external {
         require(to != address(0));
         require(to != address(this));
         require(allTokens[tokenId].owner == msg.sender);
+
+        tokenAmount[msg.sender] -= 1;
+        tokenAmount[to] += 1;
+        allTokens[tokenId].owner = to;
+
+        emit Transfer(msg.sender, to, tokenId);
     }
 
 }
