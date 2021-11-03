8b928ec6765c415e815eb171707c59ec70fe8298
EmmerichS <emschmolli@hotmail.com>
2021-11-03 08:53:34 +0100

    Wrote transfreFrom()

--- contracts/Kittycontract.sol
+++ contracts/Kittycontract.sol
@@ -149,5 +149,24 @@ contract Kittycontract is IERC721, Ownable {
     /*function owns(address claimant, uint tokenId) internal view returns(bool) {
         return ownership[tokenId] == claimant;
     }*/
+
+    /// @notice Transfer ownership of an NFT -- THE CALLER IS RESPONSIBLE
+    ///  TO CONFIRM THAT `_to` IS CAPABLE OF RECEIVING NFTS OR ELSE
+    ///  THEY MAY BE PERMANENTLY LOST
+    /// @dev Throws unless `msg.sender` is the current owner, an authorized
+    ///  operator, or the approved address for this NFT. Throws if `_from` is
+    ///  not the current owner. Throws if `_to` is the zero address. Throws if
+    ///  `_tokenId` is not a valid NFT.
+    /// @param _from The current owner of the NFT
+    /// @param _to The new owner
+    /// @param _tokenId The NFT to transfer
+    function transferFrom(address _from, address _to, uint256 _tokenId) external {
+
+        require(ownership[_tokenId] == msg.sender || approveToken[_tokenId] == msg.sender 
+            || operatorApproval[_from][msg.sender] == true, "Not owner, nor approved, nor operator");
+        require(_tokenId < allTokens.length, "Token does not exist");
+
+        _transfer(_from, _to, _tokenId);
+    }
 }    
 
