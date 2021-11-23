ebdd7a9cc857a3625baee946a1740fb7c472c8fc
EmmerichS <emschmolli@hotmail.com>
2021-11-23 17:19:18 +0100

    Added connect button functionality

--- client/index.js
+++ client/index.js
@@ -2,9 +2,41 @@ var web3js = new Web3(Web3.givenProvider);
 
 var instance;
 var user;
-var contractAddress = "0x8A0A28e20d8A5f9e297B3D22606CF168BD663e55";
+var contractAddress = "0x508bA0EFC43526aC0F17F5Ddeed79Fa4Fc30F0c4";
 
 $(document).ready(function() {
+
+
+    window.ethereum.enable().then(function(accounts) {
+
+        
+        //Creating an instance of the contract
+        instance = new web3js.eth.Contract(abi, contractAddress, {from: accounts[0]})
+
+        user = accounts[0];
+
+        console.log(instance)
+        
+
+        instance.events.Birth()
+        .on('data', function(event) {
+
+            //Here I console the DNA (genes), but only get a partial value.
+            console.log(event.returnValues.genes); 
+
+            alert("Congratulations Em!!! Your cat has been created! And here are all the stats:" + JSON.stringify(event.returnValues, null, 4));
+
+            console.log(event.returnValues);
+        })
+        .on('error', function(error) {
+            console.log(error);
+        })
+        
+
+    })
+})
+
+function connect() {
     window.ethereum.enable().then(function(accounts) {
 
         //Creating an instance of the contract
@@ -20,7 +52,8 @@ $(document).ready(function() {
             //Here I console the DNA (genes), but only get a partial value.
             console.log(event.returnValues.genes); 
 
-            alert("Congratulations Em!!! Your cat has been created! And here are all the stats:" + JSON.stringify(event.returnValues, null, 4));
+            alert("Congratulations Em!!! Your cat has been created! And here are all the stats:" + 
+            JSON.stringify(event.returnValues, null, 4));
 
             console.log(event.returnValues);
         })
@@ -29,4 +62,4 @@ $(document).ready(function() {
         })
 
     })
-})
+}
