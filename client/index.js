121f3f9309e48da6447dd55fd9745a787ffc3f4f
EmmerichS <emschmolli@hotmail.com>
2021-11-29 10:25:36 +0100

    Tidied up the code

--- client/index.js
+++ client/index.js
@@ -4,8 +4,11 @@ var instance;
 var user;
 var contractAddress = "0xc2B1512EAEEfe984a9bb5C85158cbD57Ac91b060";
 
-function getAccount() {
-   
+$(document).ready(function() {
+    checkAccount();
+})
+
+function checkAccount() {   
     ethereum.request({ method: 'eth_accounts' }).then(function(accounts) {
         let account = accounts[0]; 
         console.log(account + " Waddup " + " " + localStorage.getItem("currentAccount"));
@@ -16,13 +19,10 @@ function getAccount() {
 }
 
 function connect() {
-
     ethereum.request({ method: 'eth_requestAccounts' })
     .then(function(accounts) {
-
         instance = new web3js.eth.Contract(abi, contractAddress, {from: accounts[0]})
         user = accounts[0];
-
         localStorage.setItem("currentAccount", accounts[0]);
 
         instance.events.Birth()
@@ -32,39 +32,10 @@ function connect() {
             })
             .on('error', function(error) {
                 console.log(error);
-            })
-            
+            })            
         })
     .catch((error) => {
         console.log(error);
-    }) 
-    
+    })     
 }
-
-$(document).ready(function() {
-
-    getAccount();
-
-    /*window.ethereum.enable().then(function(accounts) {
-       
-        //Creating an instance of the contract
-        instance = new web3js.eth.Contract(abi, contractAddress, {from: accounts[0]})
-        user = accounts[0];
-        console.log(instance)
-        
-        instance.events.Birth()
-        .on('data', function(event) {
-
-            //Here I console the DNA (genes), but only get a partial value.
-            console.log(event.returnValues.genes); 
-
-            alert("Congratulations Em!!! Your cat has been created! And here are all the stats:" + JSON.stringify(event.returnValues, null, 4));
-
-            console.log(event.returnValues);
-        })
-        .on('error', function(error) {
-            console.log(error);
-        })
-    })*/
-})
 
