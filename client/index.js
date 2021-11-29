73ff8c2d3d69e2680ebc145f25f2ba26848ffabd
EmmerichS <emschmolli@hotmail.com>
2021-11-29 10:22:44 +0100

    Finally fixed the login problem with Metamask. If I switch pages and come back to the Factory page I am still logged in and don't have to reconnect every time

--- client/index.js
+++ client/index.js
@@ -2,37 +2,43 @@ var web3js = new Web3(Web3.givenProvider);
 
 var instance;
 var user;
-var contractAddress = "0xc8c62d7e1b887335d121dF9Bb888355A71bf19E5";
+var contractAddress = "0xc2B1512EAEEfe984a9bb5C85158cbD57Ac91b060";
 
 function getAccount() {
    
     ethereum.request({ method: 'eth_accounts' }).then(function(accounts) {
-        let account = accounts[0];
-        console.log(account + " Waddup");
+        let account = accounts[0]; 
+        console.log(account + " Waddup " + " " + localStorage.getItem("currentAccount"));
+        if(account == localStorage.getItem("currentAccount")) {
+            connect();
+        }
     });
 }
 
-  function connect() {
+function connect() {
 
-    ethereum.request({ method: 'eth_requestAccounts' }).then(function(accounts) {
+    ethereum.request({ method: 'eth_requestAccounts' })
+    .then(function(accounts) {
 
-        //Creating an instance of the contract
         instance = new web3js.eth.Contract(abi, contractAddress, {from: accounts[0]})
-
         user = accounts[0];
-        currentAccount = user;
 
-        console.log(instance)
+        localStorage.setItem("currentAccount", accounts[0]);
 
         instance.events.Birth()
-        .on('data', function(event) {
-            alert("Congratulations Em!!! Your cat has been created! And here are all the stats:" + 
-            JSON.stringify(event.returnValues, null, 4));
+            .on('data', function(event) {
+                alert("Congratulations Em!!! Your cat has been created! And here are all the stats:" + 
+                JSON.stringify(event.returnValues, null, 4));
+            })
+            .on('error', function(error) {
+                console.log(error);
+            })
+            
         })
-        .on('error', function(error) {
-            console.log(error);
-        })
-    })
+    .catch((error) => {
+        console.log(error);
+    }) 
+    
 }
 
 $(document).ready(function() {
